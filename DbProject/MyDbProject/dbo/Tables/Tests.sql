51b8df6e2af1c2faae4c59c00db863b74b2818c7
Chad Michel <cmichel@dontpaniclabs.com>
2018-01-01 17:15:16 -0600

    add db project

---
+++ DbProject/MyDbProject/dbo/Tables/Tests.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Tests]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ DbProject/MyDbProject/dbo/Tables/TimeLogItems.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TimeLogItems] (
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX)  NULL,
+    [User]        NVARCHAR (MAX)  NULL,
+    [Project]     NVARCHAR (MAX)  NULL,
+    [Minutes]     DECIMAL (18, 2) NOT NULL,
+    [Date]        DATETIME        NOT NULL,
+    [Description] NVARCHAR (MAX)  NULL,
+    [Week]        INT             NOT NULL,
+    [Year]        INT             NOT NULL,
+    [ExternalId]  NVARCHAR (MAX)  NULL,
+    CONSTRAINT [PK_dbo.TimeLogItems] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
