bf41c764a505b9e48a6d586b28b3adc4973df290
Chad Michel <cmichel@dontpaniclabs.com>
2018-01-03 11:43:10 -0600

    add code first migrations

--- CodeFirstWithMigrations/DatabaseLayer/MyContext.cs
+++ CodeFirstWithMigrations/DatabaseLayer/MyContext.cs
@@ -10,10 +10,7 @@ namespace DatabaseLayer
     public class MyContext : DbContext
     {
         public MyContext() : base("TimeDb")
-        {
-            // Disable db migration
-            Database.SetInitializer<MyContext>(null);
-        }
+        { }
 
         public DbSet<TimeLogItem> TimeLogItems { get; set; }
     }
---
+++ CodeFirstWithMigrations/SimpleClient/Program.cs
@@ -0,0 +1,82 @@
+﻿using CsvHelper;
+using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvData = ReadFile("sample.csv");
+
+            using (var context = new MyContext())
+            {
+                foreach (var row in csvData)
+                {
+                    var item = new TimeLogItem()
+                    {
+                        Date = row.Date,
+                        Minutes = row.Minutes,
+                        Week = row.Week,
+                        Year = row.Year,
+                        ExternalId = row.Id,
+                        Project = row.Project,
+                    };
+                    context.TimeLogItems.Add(item);                    
+                }
+                context.SaveChanges();
+            }
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+
+        static CsvData[] ReadFile(string path)
+        {
+            var csvData = new List<CsvData>();
+
+            using (var textReader = File.OpenText(@"sample.csv"))
+            using (var csv = new CsvReader(textReader))
+            {
+                DateTimeFormatInfo dfi = DateTimeFormatInfo.CurrentInfo;
+                Calendar cal = dfi.Calendar;
+
+
+                while (csv.Read())
+                {
+                    var id = csv.GetField<string>(0);
+                    var name = csv.GetField<string>(1);
+                    var userId = csv.GetField<string>(4);
+                    var project = csv.GetField<string>(6);
+                    var minutes = csv.GetField<decimal>(5);
+                    var date = csv.GetField<DateTime>(3);
+
+                    var week = cal.GetWeekOfYear(date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday);
+                    int year = date.Year;
+
+                    Console.WriteLine($"{id} {name} {userId} {project} {minutes} {week}");
+
+                    var data = new CsvData()
+                    {
+                        Id = id,
+                        Date = date,
+                        Minutes = minutes,
+                        Week = week,
+                        Year = year,
+                        Project = project,
+                    };
+                    csvData.Add(data);
+                }
+            }
+
+            return csvData.ToArray();
+        }
+    }
+}
